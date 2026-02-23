4aad09f199d5b5345a0abe1fc8034ad01e3dae6d
Shahzaib Saeed <shahzaibsaeed077@gmail.com>
2026-02-23 22:56:49 +0500

    fxi logoout

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -2,18 +2,18 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { AuthService } from '../../service/auth-service.service';
 import { RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [CommonModule,RouterLink],
+  imports: [CommonModule, RouterLink],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
   sidebarOpen = false;
   accountOpen = false;
-  constructor(public auth: AuthService) {}
-  toggleAccount() {
+  constructor(public auth: AuthService, private router: Router) { } toggleAccount() {
     this.accountOpen = !this.accountOpen;
   }
 
@@ -23,6 +23,9 @@ export class SidebarComponent {
   }
   logout(){
   this.auth.logout();
+
   this.onNavigate();
+
+  this.router.navigate(['/']);   // redirect page
 }
 }
