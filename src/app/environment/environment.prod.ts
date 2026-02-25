652e81715bd3ebaf6524a8fd778c9f6f25440b39
Shahzaib Saeed <shahzaibsaeed077@gmail.com>
2026-02-25 23:29:34 +0500

    fix prod url

--- src/app/environment/environment.prod.ts
+++ src/app/environment/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-//   apiUrl: 'https://mycopyrightlybackend-d4htk.ondigitalocean.app/api',
-  apiUrl:'http://localhost:5000/api',
+  apiUrl: 'https://mycopyrightlybackend-d4htk.ondigitalocean.app/api',
+  // apiUrl:'http://localhost:5000/api',
   stripePublishableKey:'pk_test_51R4haHLIxobea5p9MB1Po7qydXzckU1Xnv5PIgvDoEYnp9CwT4UEm23xpegbORk4lELoAUH3ietslmpte7GqrXJ300BeDPsC0l'
 };
